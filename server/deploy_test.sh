b510000770963e273faced626c99d6ad1bbc4f27
Peter Phanouvong <peterphanouvong@gmail.com>
2021-08-12 09:11:57 +1000

    setting up the testing env

---
+++ server/deploy_test.sh
@@ -0,0 +1,7 @@
+
+echo What should the version be?
+read VERSION
+
+docker build -t peterphanouvong/sprt-test:$VERSION .
+docker push peterphanouvong/sprt-test:$VERSION
+ssh root@134.209.97.7 "docker pull peterphanouvong/sprt-test:$VERSION && docker tag peterphanouvong/sprt-test:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
