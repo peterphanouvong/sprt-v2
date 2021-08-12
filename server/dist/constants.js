f89d5840379946bce8cf6fde89eb1d4ca47279b3
Peter Phanouvong <peterphanouvong@gmail.com>
2021-08-12 13:05:02 +1000

    test

--- server/dist/constants.js
+++ server/dist/constants.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.FORGET_PASSWORD_PREFIX = exports.COOKIE_NAME = exports.__test__ = exports.__prod__ = void 0;
 exports.__prod__ = process.env.NODE_ENV === "production";
-exports.__test__ = process.env.NODE_ENV === "test";
+exports.__test__ = process.env.NODE_ENV === "test" || process.env.ENV === "test";
 exports.COOKIE_NAME = "qid";
 exports.FORGET_PASSWORD_PREFIX = "forget-password:";
 //# sourceMappingURL=constants.js.map
--- server/src/constants.ts
+++ server/src/constants.ts
@@ -1,5 +1,6 @@
 export const __prod__ = process.env.NODE_ENV === "production";
-export const __test__ = process.env.NODE_ENV === "test";
+export const __test__ =
+  process.env.NODE_ENV === "test" || process.env.ENV === "test";
 export const COOKIE_NAME = "qid";
 
 export const FORGET_PASSWORD_PREFIX = "forget-password:";
