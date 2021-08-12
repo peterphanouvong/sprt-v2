31440d03398e0a72453d3f727a60470b82d216c9
Peter Phanouvong <peterphanouvong@gmail.com>
2021-08-12 11:18:41 +1000

    update deploy_test script

--- server/deploy_test.sh
+++ server/deploy_test.sh
@@ -4,4 +4,4 @@ read VERSION
 
 docker build -t peterphanouvong/sprt-test:$VERSION .
 docker push peterphanouvong/sprt-test:$VERSION
-ssh root@134.209.97.7 "docker pull peterphanouvong/sprt-test:$VERSION && docker tag peterphanouvong/sprt-test:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+ssh root@134.209.97.7 "docker pull peterphanouvong/sprt-test:$VERSION && docker tag peterphanouvong/sprt-test:$VERSION dokku/test-api:$VERSION && dokku deploy test-api $VERSION"
