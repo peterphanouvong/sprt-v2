3f8da6d3a00c1e6cc3f747abdbe1234b987f201f
Peter Phanouvong <peterphanouvong@gmail.com>
2021-08-14 21:55:38 +1000

    list styling

--- client/src/components/AccordionUsers.tsx
+++ client/src/components/AccordionUsers.tsx
@@ -11,6 +11,7 @@ import {
 } from "@chakra-ui/react";
 import React from "react";
 import { User } from "../generated/graphql";
+import { MetaDataText } from "./ MetaDataText";
 
 interface Props {
   userType: string;
@@ -22,18 +23,34 @@ const AccordionUsers: React.FC<Props> = ({ userType, userList }) => {
     <Box mt={4}>
       <Accordion allowToggle>
         <AccordionItem>
-          <AccordionButton>
-            <Box flex='1' textAlign='left'>
-              <Text fontWeight='medium'>{userType}</Text>
+          <AccordionButton
+            _hover={{
+              backgroundColor: "gray.50",
+            }}
+          >
+            <Box flex="1" textAlign="left">
+              <Text fontWeight="medium">{userType}</Text>
             </Box>
             <AccordionIcon />
           </AccordionButton>
-          <AccordionPanel>
-            <UnorderedList>
-              {userList.map((attendee) => (
-                <ListItem key={attendee.id}>{attendee.username}</ListItem>
-              ))}
-            </UnorderedList>
+          <AccordionPanel p={0}>
+            {/* <UnorderedList> */}
+            {userList.map((attendee) => (
+              <ListItem
+                paddingY={1}
+                paddingX={4}
+                borderTop="1px solid"
+                _hover={{
+                  backgroundColor: "gray.50",
+                }}
+                borderColor="gray.100"
+                listStyleType="none"
+                key={attendee.id}
+              >
+                <MetaDataText>{attendee.username}</MetaDataText>
+              </ListItem>
+            ))}
+            {/* </UnorderedList> */}
           </AccordionPanel>
         </AccordionItem>
       </Accordion>
