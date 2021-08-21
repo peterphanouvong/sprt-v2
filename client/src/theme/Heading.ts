a516421fe7ed6b82ca39906798171341a1c269ce
Peter Phanouvong <peterphanouvong@gmail.com>
2021-08-21 12:51:41 +1000

    typography

---
+++ client/src/theme/Heading.ts
@@ -0,0 +1,22 @@
+export const Heading = {
+  variants: {
+    h1: {
+      fontSize: "2.488rem",
+    },
+    h2: {
+      fontSize: "2.074rem",
+    },
+    h3: {
+      fontSize: "1.728rem",
+    },
+    h4: {
+      fontSize: "1.44rem",
+    },
+    h5: {
+      fontSize: "1.2rem",
+    },
+    h6: {
+      fontSize: "1rem",
+    },
+  },
+};
---
+++ client/src/theme/Text.ts
@@ -0,0 +1,29 @@
+export const Text = {
+  variants: {
+    "body-1": {
+      fontSize: "1.2rem",
+    },
+    "body-2": {
+      fontSize: "1rem",
+    },
+    "body-3": {
+      fontSize: "0.833rem",
+    },
+    meta: {
+      color: "blackAlpha.600",
+      fontSize: "0.694rem",
+    },
+    footer: {
+      fontSize: "0.579rem",
+    },
+    caption: {
+      fontSize: "0.833rem",
+      textTransform: "uppercase",
+      color: "blackAlpha.600",
+    },
+    label: {
+      fontSize: "0.833rem",
+      color: "blackAlpha.600",
+    },
+  },
+};
