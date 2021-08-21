f72fee4d23688d9229b98e61dc74b888ad630d07
Peter Phanouvong <peterphanouvong@gmail.com>
2021-08-21 20:10:16 +1000

    event filter ui done!

--- client/src/components/PublicFeed.tsx
+++ client/src/components/PublicFeed.tsx
@@ -1,8 +1,16 @@
 import { Spinner } from "@chakra-ui/react";
-import React from "react";
-import { Event, useFeedQuery } from "../generated/graphql";
-import { RenderPrettyJSON } from "../utils/renderPrettyJSON";
+import React, { useState } from "react";
+import {
+  Club,
+  Event,
+  PublicityType,
+  useFeedQuery,
+  useMeQuery,
+  usePublicityTypesQuery,
+} from "../generated/graphql";
+// import { RenderPrettyJSON } from "../utils/renderPrettyJSON";
 import { EventList } from "./EventList";
+import { EventListFilter } from "./EventListFilter";
 
 interface Props {
   userId: number;
@@ -14,12 +22,49 @@ const PublicFeed: React.FC<Props> = ({ userId }) => {
       id: userId,
     },
   });
+  const [{ data: meData }] = useMeQuery();
+  const [{ data: publicityTypesData }] = usePublicityTypesQuery();
+
+  const [selectedClubs, setSelectedClubs] = useState<Record<number, boolean>>(
+    meData?.me?.followingClubs?.reduce((map, obj) => {
+      map[obj.id] = true;
+      return map;
+    }, {})!
+  );
+
+  const [selectedPublicityTypes, setSelectedPublicityTypes] = useState<
+    Record<number, boolean>
+  >(
+    publicityTypesData?.publicityTypes.reduce((map, obj) => {
+      map[obj.id] = true;
+      return map;
+    }, {})!
+  );
+
   if (!data && !fetching) return <>ya fucked it</>;
   if (!data) return <Spinner />;
+
   return (
     <>
-      <RenderPrettyJSON object={data} />
-      <EventList events={data.feed as Event[]} />
+      <EventListFilter
+        clubs={meData?.me?.followingClubs as Club[]}
+        selectedClubs={selectedClubs}
+        setSelectedClubs={setSelectedClubs}
+        publicityTypes={publicityTypesData?.publicityTypes as PublicityType[]}
+        selectedPublicityTypes={selectedPublicityTypes}
+        setSelectedPublicityTypes={setSelectedPublicityTypes}
+      />
+      {/* <RenderPrettyJSON object={selectedClubs} /> */}
+      <EventList
+        events={
+          data.feed.filter((x) => {
+            return x.clubId
+              ? selectedClubs[x.clubId] &&
+                  selectedPublicityTypes[x.publicityTypeId]
+              : selectedPublicityTypes[x.publicityTypeId];
+          }) as Event[]
+        }
+      />
     </>
   );
 };
