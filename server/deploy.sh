569a191e1513571b981dc00095267841ac8c7eb1
Peter Phanouvong <peterphanouvong@gmail.com>
2021-11-09 09:36:22 +1100

    prepping for production

--- server/deploy.sh
+++ server/deploy.sh
@@ -4,6 +4,6 @@
 echo What should the version be?
 read VERSION
 
-docker build -t peterphanouvong/sprt:$VERSION .
-docker push peterphanouvong/sprt:$VERSION
-ssh root@128.199.115.97 "docker pull peterphanouvong/sprt:$VERSION && docker tag peterphanouvong/sprt:$VERSION dokku/api:$VERSION && dokku deploy api $VERSION"
+docker build -t peterphanouvong/sprtv2:$VERSION .
+docker push peterphanouvong/sprtv2:$VERSION
+ssh root@128.199.201.202 "docker pull peterphanouvong/sprtv2:$VERSION && docker tag peterphanouvong/sprtv2:$VERSION dokku/sprtv2:$VERSION && dokku deploy sprtv2 $VERSION"
