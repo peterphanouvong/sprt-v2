d6b26f61d48d278e025b6d8abc4cf0cf2e97d7de
kevin <kevin.chau5@outlook.com>
2021-12-08 13:41:21 +1100

    changing branches

--- client/src/components/EventAttendeeWaitlist.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-interface Props {
-  isAdminView: boolean;
-}
-
-export const EventAttendeeWaitlist: React.FC<Props> = ({ isAdminView }) => {
-  return <div></div>;
-};
